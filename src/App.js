4d6f59c0e2fc4aaadf37f107ce3ae77aabdcb828
Aleksey-Danchin <aleksey.danchin@gmail.com>
2021-07-23 21:00:09 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import { BattleField, BattleFieldTable, Ship, Shot } from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Switch, Route } from "react-router-dom";
+
+import { BotPage, EditorPage, MainPage } from "./pages";
+
+const App = () => {
+	return (
+		<Switch>
+			<Route path="/editor" component={EditorPage} />
+			<Route path="/bot" component={BotPage} />
+			<Route path="/" component={MainPage} />
+		</Switch>
+	);
+	// return (
+	// 	<>
+	// 		<BattleField style={{ margin: "30px" }}>
+	// 			<BattleFieldTable />
+
+	// 			<Ship x={0} y={0} length={4} direction="row" />
+	// 			<Ship x={2} y={2} length={2} direction="column" killed />
+
+	// 			<Shot x={0} y={2} status="missed" />
+	// 			<Shot x={0} y={4} status="hitted" />
+	// 		</BattleField>
+	// 	</>
+	// );
+};
 
 export default App;
+
+/*
+<BattleField style={{ margin: "30px" }}>
+	<BattleFieldTable
+		columns={12}
+		rows={5}
+		signed={false}
+		axisX={(n) => n + 1}
+		axisY={(n) => "АБВГДЕЖЗИКЛМ"[n]}
+	/>
+</BattleField>
+*/
